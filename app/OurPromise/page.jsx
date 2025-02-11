d8a7c3a47152d865c73651daeecf528c135004bd
NYUCT Design Labs <132071678+nyuct@users.noreply.github.com>
2025-02-11 18:25:41 +0530

    refactor: update component import paths to align with app directory structure

--- app/OurPromise/page.jsx
+++ app/OurPromise/page.jsx
@@ -3,8 +3,8 @@ import { useEffect, useRef } from "react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import RootLayout from "@/app/layout";
-import RotateBottel from "@/components/RotateBottel";
-import ProductImages from "@/components/ProductImages";
+import RotateBottel from "@/app/components/RotateBottel";
+import ProductImages from "@/app/components/ProductImages";
 
 gsap.registerPlugin(ScrollTrigger);
 
